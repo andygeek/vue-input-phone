cfbabd64a3b268003cdce4bdcacefc65518ee470
andygeek <andy.eulogio@gmail.com>
2022-02-02 23:11:38 -0500

    From .ts to .js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import InputPhone from './components/InputPhone.vue'
 
 const plugin = {
-  install (Vue: any, options: any) {
+  install (Vue, options) {
     Vue.component(InputPhone.name, InputPhone)
   }
 }
+InputPhone.install = plugin.install
+
 export default InputPhone
