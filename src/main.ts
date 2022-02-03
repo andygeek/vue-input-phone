9798fbc2be935de2e349ec4b75ff33839071c171
andygeek <andy.eulogio@gmail.com>
2022-02-02 20:15:46 -0500

    Fixed typos

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import InputPhone from './components/InputPhone.vue'
 
 const plugin = {
-  install: Vue => {
+  install (Vue: any, options: any) {
     Vue.component(InputPhone.name, InputPhone)
   }
 }
-
-InputPhone.install = plugin.install
-
 export default InputPhone
